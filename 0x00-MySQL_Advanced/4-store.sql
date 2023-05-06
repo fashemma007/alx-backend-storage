d88b48f9e7e4db05c3450f20e638d153df6b3f3e
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-05-06 10:09:13 +0100

    fix:

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,13 +1,16 @@
 -- This SQL script creates a trigger named 'item_sold' that runs after an insert on the 'orders' table.
 DROP TRIGGER IF EXISTS item_sold;
+
 -- DELIMITER $$ indeicates delim change from ; to $$
-DELIMITER $$ CREATE TRIGGER item_sold
-AFTER -- shld run after
-INSERT ON orders FOR EACH ROW BEGIN -- an insert statement on orders table
-UPDATE items -- update items table
-SET quantity = quantity - NEW.number -- subtract number of orders from qtty
-WHERE name = NEW.item_name;
--- from the newly added order name
+DELIMITER $$
+
+CREATE TRIGGER item_sold
+	AFTER INSERT
+	ON orders FOR EACH ROW -- run after an insert statement on orders table
+BEGIN
+	UPDATE items -- update items table
+	SET quantity = quantity - NEW.number -- subtract number of orders from qtty
+	-- from the newly added order name
+	WHERE name = NEW.item_name;
 END $$
-DELIMITER ;
--- disable newly set delimiter
+DELIMITER ; -- disable newly set delimiter
