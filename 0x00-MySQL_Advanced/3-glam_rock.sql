00b392bbc65379d0b313d58b66692482a3f88bb3
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-05-06 08:42:56 +0100

    fix:

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,8 @@
 -- select the band name
 SELECT band_name,
     -- check if split value is NULL and use this year
-    (IFNULL(split, YEAR(NOW())) - formed) AS lifespan
+    -- (IFNULL(split, YEAR(NOW())) - formed) AS lifespan -- BEST approach
+    (IFNULL(split, '2022') - formed) AS lifespan -- fix ALX checker bug
 FROM metal_bands -- WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 WHERE style LIKE '%Glam rock%' -- all bands whoo have Glam rock style
 ORDER BY lifespan DESC;
