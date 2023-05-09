38bc2e70e9a079d285adcb23648b0a286ad9bdd5
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-05-09 21:01:57 +0100

    feat:

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Write a Python function that lists all documents in a collection and
+returns an empty list if no document in the collection mongo_collection will be
+the pymongo collection object
+"""
+
+import pymongo
+
+
+def list_all(mongo_collection):
+    """Return list of all docs in collection"""
+    if not mongo_collection:
+        return []
+    docs = mongo_collection.find()
+    return [doc for doc in docs]
